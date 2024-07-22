9d6145c55606214be4586ce370e748803911b9b4
Jaeyoon Kim <201802084@g.cnu.ac.kr>
2024-07-22 21:09:23 +0900

    search histroy attempt 1

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,52 +1,59 @@
 let searchOutcome = "";
 let isSidePanelOpen = false;
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) { // content_script의 요청을 받으면 요청받은 단어를 python기반 wiki검색 서버에 전달
-                                                                          // 검색 결과를 받으면 side_panel에 검색 결과를 채워서 제공
-    let message = {request:request.request};
-    if(request.action === "wikiSearch") {
+let searchHistory = [];
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let message = { request: request.request };
+    if (request.action === "wikiSearch") {
         postData('http://127.0.0.1:8888/', message).then((response) => {
-            if(typeof(response) === 'string') {
+            if (typeof(response) === 'string') {
                 sendResponse(response);
                 searchOutcome = response;
-            }else {
+                addToSearchHistory(request.request);
+            } else {
                 sendResponse("error occurred in service-worker");
             }
-            }).catch((error) => {
+        }).catch((error) => {
             sendResponse("error occurred");
         });
-    }else if(request.action === "openSideBar"){
-        chrome.sidePanel.open({tabId : sender.tab.id});
-        if(!isSidePanelOpen) {
+    } else if (request.action === "openSideBar") {
+        chrome.sidePanel.open({ tabId: sender.tab.id });
+        if (!isSidePanelOpen) {
             isSidePanelOpen = true;
-        }else {
-            chrome.runtime.sendMessage({request: searchOutcome, action: "reFill"})
+        } else {
+            chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
         }
-    }else if(request.action === "sideBarText") {
+    } else if (request.action === "sideBarText") {
         sendResponse(searchOutcome);
-    }else if(request.action === 'closeSideBar') {
+    } else if (request.action === "closeSideBar") {
         isSidePanelOpen = false;
+    } else if (request.action === "getHistory") {
+        console.log("Sending history:", searchHistory);  // 로그 추가
+        sendResponse(searchHistory);
     }
 
     return true;
 });
 
-async function postData(url = "", data = {}){
+function addToSearchHistory(word) {
+    if (!searchHistory.includes(word)) {
+        searchHistory.push(word);
+    }
+}
+
+async function postData(url = "", data = {}) {
     const response = await fetch(url, {
-        method:"POST",
+        method: "POST",
         mode: "cors",
         cache: "no-cache",
         credentials: "same-origin",
         headers: {
-            "Content-Type":"application/json"
-            },
+            "Content-Type": "application/json"
+        },
         redirect: "follow",
         referrerPolicy: "no-referrer",
         body: JSON.stringify(data)
     });
-        
+
     return response.text();
 }
-
-
-//send된 단어를 받아서 파이썬 코드(wikipedia 검색 코드)에 넘겨주고 검색 결과를 받음
-//side panel open 후 검색 결과를 창에 출력
--- side_panel.js
+++ side_panel.js
@@ -2,17 +2,18 @@ let wordDiv = document.getElementById('word');
 let summaryDiv = document.getElementById('summary');
 let explainDiv = document.getElementById('explain');
 let relatedList = document.getElementById('relatedList');
+let historyList = document.getElementById('historyList');
 
 function relateLi(relatedStr) {
     let relatedDict = relatedStr.replace(/^({|})+|({|})+$/g, '');
     relatedDict = relatedDict.split('),')
     for (let val of relatedDict) {
-        val = val+')'
+        val = val + ')'
         let li = document.createElement('LI');
         let textNode = document.createTextNode(val);
         li.appendChild(textNode);
         relatedList.appendChild(li);
-    }   
+    }
 }
 
 function putDataInDiv(json_data) {
@@ -24,18 +25,43 @@ function putDataInDiv(json_data) {
     relateLi(relatedStr);
 }
 
-chrome.runtime.sendMessage({action:"sideBarText"}, (response) => {
+function displayHistory(history) {
+    console.log("Received history:", history);  // 로그 추가
+    historyList.innerHTML = '';
+    history.forEach(word => {
+        let li = document.createElement('LI');
+        let link = document.createElement('a');
+        link.href = '#';
+        link.innerText = word;
+        link.onclick = function () {
+            chrome.runtime.sendMessage({ request: word, action: "wikiSearch" }, function (response) {
+                if (response !== "error occurred") {
+                    console.log(response);
+                    requestOpenSideBar();
+                }
+            });
+        };
+        li.appendChild(link);
+        historyList.appendChild(li);
+    });
+}
+
+chrome.runtime.sendMessage({ action: "sideBarText" }, (response) => {
     putDataInDiv(response);
-})
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) { 
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     let message = request.request;
-    if(request.action === "reFill") {
+    if (request.action === "reFill") {
         putDataInDiv(message);
     }
-    
     return true;
 });
 
 addEventListener("beforeunload", (event) => {
-    chrome.runtime.sendMessage({action:"closeSideBar"})
+    chrome.runtime.sendMessage({ action: "closeSideBar" });
+});
+
+chrome.runtime.sendMessage({ action: "getHistory" }, (history) => {
+    displayHistory(history);
 });
