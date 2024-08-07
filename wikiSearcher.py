e1ed2e21f2da578c845b386c6a88f5007cb06f23
unknown <201803851@o.cnu.ac.kr>
2024-08-07 20:01:29 +0900

    add multi-threading

--- wikiSearcher.py
+++ wikiSearcher.py
@@ -10,6 +10,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer #need 'pip install s
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
 from bs4 import BeautifulSoup
+import threading
 
 WIKI = wikipediaapi.Wikipedia('201803851@o.cnu.ac.kr','ko')
 ADDRESS = '127.0.0.1'
@@ -50,31 +51,54 @@ def clean_text(text):
     text = re.sub(r'{[^{}]+}', '', text)  # 중괄호 내부 내용 제거
 
     return text
+
 def handle_homonym(links, original_text):
     best_match = None
     highest_similarity = 0
     vectorizer = TfidfVectorizer()
-    origin_transform = vectorizer.fit_transform([original_text])
+    origin_transform = vectorizer.fit_transform(original_text)
     original_vector = origin_transform.toarray()[0]
+    link_unhomonym = []
+    threadArr = []
+    
+    def thread_function(link, original_vector, id, vectorizer):
+        linkReader = WIKI.page(link)
+        link_text = linkReader.text
+        link_vector = vectorizer.transform([link_text]).toarray()[0]
+        similarity = cosine_similarity([original_vector], [link_vector])[0][0]
+        print(linkReader.title+"||"+str(similarity))
+        similarities[id] = similarity
 
     for link in links:
         if '동음이의' in link or '동명이인' in link:
             continue
-        
-        linkReader = WIKI.page(link)
-        if linkReader.exists():
-            link_text = linkReader.text
-            link_vector = vectorizer.transform([link_text]).toarray()[0]
-            similarity = cosine_similarity([original_vector], [link_vector])[0][0]
+        else:
+            link_unhomonym.append(link)
+            
+    similarities = [0.0]*len(link_unhomonym)
+    idNum = 0    
+    for link in link_unhomonym:    
+    #     linkReader = WIKI.page(link)
+    #     if linkReader.exists():
+    #         link_text = linkReader.text
+    #         link_vector = vectorizer.transform([link_text]).toarray()[0]
+    #         similarity = cosine_similarity([original_vector], [link_vector])[0][0]
+    #         print(linkReader.title+"||"+str(similarity))
 
-            if similarity > highest_similarity:
-                highest_similarity = similarity
-                best_match = link
+    #         if similarity > highest_similarity:
+    #             highest_similarity = similarity
+    #             best_match = link
+        thread = threading.Thread(target = thread_function, args = (link, original_vector, idNum, vectorizer))
+        thread.start()
+        threadArr.append(thread)
+        idNum = idNum+1
+    
+    for thread in threadArr:
+        thread.join()
 
-    if best_match:
-        return best_match
-    else:
-        return 'tree (명령어)'  # 적절한 기본값을 반환하는 부분으로 보임
+    # if best_match:
+    best_match = link_unhomonym[similarities.index(max(similarities))]
+    return best_match
 
 def re_search(wikiReader, original_text):
     links = wikiReader.links
@@ -88,16 +112,12 @@ def search_wiki(data):
     usePara = data['usePara']
     wikiReader = WIKI.page(text)
     if(wikiReader.exists()):
+        if check_homonym(wikiReader):
+            wikiReader = re_search(wikiReader, usePara)
         print(wikiReader.text)
         word = text
         summary = clean_text(wikiReader.summary)
         explain = clean_text(wikiReader.text)
-        if check_homonym(wikiReader):
-            wikiReader = re_search(wikiReader, wikiReader.text)
-        word = text
-        print(wikiReader.text)
-        summary = wikiReader.summary
-        explain = wikiReader.text
         related = wikiReader.links
         related = related2str(related)
         result = wiki_data_json(word, summary, explain, related)
