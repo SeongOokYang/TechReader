0e6bdd109de2017f2202f5bf02776f21b6faacdd
unknown <201803851@o.cnu.ac.kr>
2024-08-10 00:28:35 +0900

    handle sidepanel open error

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,35 +1,56 @@
 let searchOutcome = "";
 let isSidePanelOpen = false;
 let searchHistory = [];
+const url = 'http://127.0.0.1:8888/'
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     let message = { request: request.request };
     if (request.action === "wikiSearch") {
-        postData('http://127.0.0.1:8888/', message).then((response) => {
-            if (typeof(response) === 'string') {
-                sendResponse(response);
-                searchOutcome = response;
-                addToSearchHistory(request.request);
-                if (isSidePanelOpen) {
-                    chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
+        if(!isSidePanelOpen) {
+            chrome.sidePanel.open({ tabId: sender.tab.id });
+            isSidePanelOpen = true;
+            postData(url, message).then((response) => {
+                if (typeof(response) === 'string') {
+                    sendResponse(response);
+                    searchOutcome = response;
+                    addToSearchHistory(request.request);
+                    if (isSidePanelOpen) {
+                        chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
+                    }
+                } else {
+                    sendResponse("error occurred in service-worker");
                 }
-            } else {
-                sendResponse("error occurred in service-worker");
-            }
-        }).catch((error) => {
-            sendResponse("error occurred");
-        });
+            }).catch((error) => {
+                sendResponse("error occurred");
+            });
+        }else {
+            postData(url, message).then((response) => {
+                if (typeof(response) === 'string') {
+                    sendResponse(response);
+                    searchOutcome = response;
+                    addToSearchHistory(request.request);
+                    if (isSidePanelOpen) {
+                        chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
+                    }
+                } else {
+                    sendResponse("error occurred in service-worker");
+                }
+            }).catch((error) => {
+                sendResponse("error occurred");
+            });
+            chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
+        }
     } else if(request.action === "get_text") {
         postData('http://127.0.0.1:8888/get_text', message).then((response) => {
             sendResponse(response)
         })
-    }else if (request.action === "openSideBar") {
-        chrome.sidePanel.open({ tabId: sender.tab.id });
-        if (!isSidePanelOpen) {
-            isSidePanelOpen = true;
-        } else {
-            chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
-        }
+    // }else if (request.action === "openSideBar") {
+    //     chrome.sidePanel.open({ tabId: sender.tab.id });
+    //     if (!isSidePanelOpen) {
+    //         isSidePanelOpen = true;
+    //     } else {
+    //         chrome.runtime.sendMessage({ request: searchOutcome, action: "reFill" });
+    //     }
     } else if (request.action === "sideBarText") {
         sendResponse(searchOutcome);
     } else if (request.action === "closeSideBar") {
@@ -42,6 +63,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     return true;
 });
 
+
 function addToSearchHistory(word) {
     const includesArray = (hist, comp) => {
         return hist.some(item => item.text === comp.text);
@@ -52,18 +74,35 @@ function addToSearchHistory(word) {
 }
 
 async function postData(url = "", data = {}) {
-    const response = await fetch(url, {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(data)
-    });
+    const controller = new AbortController();
+    const signal = controller.signal;
 
-    return response.text();
+    const timeoutID = setTimeout(() => {
+        controller.abort();
+    }, 180000);
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            credentials: "same-origin",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            redirect: "follow",
+            referrerPolicy: "no-referrer",
+            body: JSON.stringify(data),
+            signal
+        });
+        
+        clearTimeout(timeoutID)
+        return response.text();
+    } catch(error) {
+        if(error.name === 'AbortError') {
+            throw new Error('Request Time Out');
+        }
+        throw error;
+    }
+    
 }
